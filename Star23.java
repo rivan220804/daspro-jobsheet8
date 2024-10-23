f2fc346c9e3388fad5cedf83aacb63a89b1c3856
rivan220804 <fadillahrivan25@gmail.com>
2024-10-23 18:53:17 +0700

    Star23

---
+++ Star23.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+/**
+ * Star23
+ */
+public class Star23 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan nilai N = ");
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++) {
+            System.out.print("*");
+        }
+    }
+}
