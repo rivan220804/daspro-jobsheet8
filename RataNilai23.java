090af44ab489063a98ea3ac418e388d27b4d6227
rivan220804 <fadillahrivan25@gmail.com>
2024-10-23 22:37:28 +0700

    RataNilai23

--- RataNilai23.java
+++ RataNilai23.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
-/**
- * RataNilai23
- */
-public class RataNilai23 {
 
+public class RataNilai23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (j=1; j<=5; i++) {
+
+        int i = 1, j = 1, nilaiMhs;
+        double nilai, totalNilai = 0, rataNilai;
+
+        while(i<=5) {
+            totalNilai = 0;
+            System.out.println("Input nilai mahasiswa ke-" + i);
+            for(j=1; j<=5; j++) {
+                System.out.print("Nilai ke-" + j + " = ");
+                nilaiMhs = sc.nextInt();
+                totalNilai += nilaiMhs;
+            }
+            rataNilai = totalNilai/5;
+            System.out.println("rata-rata nilai mahasiswa ke " + i + " adalah " + rataNilai);
+            i++;
         }
-        for (j)
-
-
-
     }
 }
